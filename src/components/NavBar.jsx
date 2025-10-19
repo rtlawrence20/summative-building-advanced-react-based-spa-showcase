c8e3bc2de388facf63a86066a9333398f79e7e22
Robert Lawrence <29217166+rtlawrence20@users.noreply.github.com>
2025-10-18 19:30:45 -0600

    Impl. basic navigation of SPA; styling for Navbar and index.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,1 +1,35 @@
 
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../styles/NavBar.module.css";
+
+function NavBar() {
+    return (
+        <nav className={styles.navbar}>
+            <ul className={styles["nav-links"]}>
+                <li>
+                    <NavLink to="/" end className={({ isActive }) => isActive ? styles.active : ""}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/shop" className={({ isActive }) => isActive ? styles.active : ""}>
+                        Shop
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={({ isActive }) => isActive ? styles.active : ""}>
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/admin" className={({ isActive }) => isActive ? styles.active : ""}>
+                        Admin
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
