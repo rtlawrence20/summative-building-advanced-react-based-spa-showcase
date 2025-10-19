eaa1cda2999919c5a4ccce27bc1bf28e613b2c60
Robert Lawrence <29217166+rtlawrence20@users.noreply.github.com>
2025-10-18 19:50:19 -0600

    impl. logic for compoent rendering

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CoffeeProvider } from "./context/CoffeeContext";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <CoffeeProvider>
+            <App />
+        </CoffeeProvider>
+    </StrictMode>,
 )
